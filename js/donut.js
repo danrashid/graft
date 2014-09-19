9351febe368521acfcce7563ed87bb7110006713
Dan Rashid <drashid@signal.co>
2014-09-19 16:02:07 -0500

    Filling in donut hole

--- js/donut.js
+++ js/donut.js
@@ -1,9 +1,7 @@
 /* global Graft, $: false */
 'use strict';
 
-Graft.donut = function (sel, data, spaceDeg) {
-  spaceDeg = spaceDeg || 5;
-
+Graft.donut = (function() {
   function getSkewY(deg) {
     return Math.min(deg - 90, 89);
   }
@@ -19,78 +17,100 @@ Graft.donut = function (sel, data, spaceDeg) {
     };
   }
 
-  function appendSpace() {
-    $('<div class="space">')
-      .css(getTransformRules(start, spaceDeg))
-      .appendTo($donut);
-
-    start += spaceDeg;
+  function positionLabel($el) {
+    $el.css('margin-top', -$el.height() / 2);
   }
 
-  var $el = $(sel),
-    $donut = $('<div class="graft donut"><div class="hole">'),
-    total = data.reduce(function (a, b) {
-      return a + b.total;
-    }, 0),
-    start = -spaceDeg,
-    slices = data.map(function (d) {
-      return {
-        id: d.id,
-        name: d.name,
-        color: d.color,
-        total: d.total,
-        ratio: d.total / total
-      };
-    }),
-    lastSlice;
+  function bind(sel, data, spaceDeg) {
+    spaceDeg = spaceDeg || 5;
 
-  slices.sort(function (a, b) {
-    return a.total - b.total;
-  });
-
-  lastSlice = slices.pop();
-
-  slices.forEach(function (d, i) {
-    var sliceDeg = Math.floor(360 * d.ratio) - spaceDeg;
-
-    appendSpace();
-
-    $('<div class="slice">')
-      .data(d)
-      .css('background', d.color)
-      .css(getTransformRules(start, sliceDeg))
-      .appendTo($donut);
-
-    start += sliceDeg;
-
-    if (i === slices.length - 1) {
-      appendSpace();
+    function setLabel(html) {
+      $label.html(html);
+      positionLabel($label);
     }
+
+    var $el = $(sel),
+      $donut = $('<div class="graft donut">'),
+      $label = $('<div class="label">'),
+      total = data.reduce(function (a, b) {
+        return a + b.total;
+      }, 0),
+      slices = data.map(function (d) {
+        return {
+          id: d.id,
+          name: d.name,
+          color: d.color,
+          total: d.total,
+          ratio: d.total / total
+        };
+      }),
+      start = -spaceDeg;
+
+    $('<div class="hole">')
+      .appendTo($donut)
+      .append($label);
+
+    slices.sort(function (a, b) {
+      return a.total - b.total;
+    });
+
+    slices.forEach(function (d, i) {
+      var sliceDeg = Math.floor(360 * d.ratio) - spaceDeg;
+
+      if (slices.length > 1) {
+        $('<div class="space">')
+          .css(getTransformRules(start, spaceDeg))
+          .appendTo($donut);
+
+        start += spaceDeg;
+      }
+
+      $('<div class="slice">')
+        .data(d)
+        .css('background', d.color)
+        .css(i < slices.length - 1 ? getTransformRules(start, sliceDeg) : null)
+        .appendTo($donut);
+
+      start += sliceDeg;
+    });
+
+    $donut
+      .height($el.width())
+      .appendTo($el)
+      .on('click', '.slice', function () {
+        var $slice = $(this),
+          percent;
+
+        if ($slice.hasClass('inactive') || !$slice.siblings('.inactive').length) {
+          percent = Math.round($slice.data('ratio') * 10000) / 100;
+
+          $slice
+            .removeClass('inactive')
+            .siblings()
+              .addClass('inactive');
+
+          setLabel([
+            '<div class="percent">' + percent + '%</div>',
+            '<div class="name" style="color:' + $slice.data('color') + '">' + $slice.data('name') + '</div>',
+            '<div class="total">' + $slice.data('total') + '</div>'
+          ].join(''));
+        } else {
+          $slice.siblings().removeClass('inactive');
+          setLabel(total);
+        }
+      });
+
+    setLabel(total);
+  }
+
+  $(window).on('resize', function () {
+    $('.graft.donut').height(function () {
+      return $(this).width();
+    });
+    positionLabel($('.graft.donut .label'));
   });
 
-  $('<div class="slice">')
-    .data(lastSlice)
-    .css('background', lastSlice.color)
-    .appendTo($donut);
-
-  $donut
-    .height($el.width())
-    .appendTo($el);
-};
-
-$(window).on('resize', function () {
-  $('.donut').each(function () {
-    var $this = $(this);
-
-    $this.height($this.width());
-  });
-});
-
-$(document).on('click', '.slice', function (e) {
-  var $slice = $(this),
-    percent = Math.round($slice.data('ratio') * 10000) / 100,
-    html = '<div style="color:' + $slice.data('color') + '"><strong>' + percent + '%</strong></div>' +
-      '<div>' + $slice.data('name') + '</div>';
-
-  Graft.tooltip.show(e, html);
-});
+  return {
+    bind: bind
+  };
+})();
